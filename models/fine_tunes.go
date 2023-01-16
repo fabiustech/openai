f1d08517270d7436e6183c888e87f918b9e5681f
Andy Day <aday@fabius.io>
2023-01-16 08:05:01 -0800

    add enum methods

--- models/fine_tunes.go
+++ models/fine_tunes.go
@@ -3,9 +3,10 @@ package models
 type FineTune int
 
 const (
+	UnknownFineTune FineTune = iota
 	// Davinci most capable of the older versions of the GPT-3 models
 	// and is intended to be used with the fine-tuning endpoints.
-	Davinci FineTune = iota
+	Davinci
 	// Curie is very capable, but faster and lower cost than Davinci. It is
 	// an older version of the GPT-3 models and is intended to be used with
 	// the fine-tuning endpoints.
@@ -19,6 +20,29 @@ const (
 	// models and is intended to be used with the fine-tuning endpoints.
 	Ada
 )
+
+// String implements the fmt.Stringer interface.
+func (f FineTune) String() string {
+	return fineTuneToString[f]
+}
+
+// MarshalText implements the encoding.TextMarshaler interface.
+func (f FineTune) MarshalText() ([]byte, error) {
+	return []byte(f.String()), nil
+}
+
+// UnmarshalText implements the encoding.TextUnmarshaler interface.
+// On unrecognized value, it sets |e| to Unknown.
+func (f *FineTune) UnmarshalText(b []byte) error {
+	if val, ok := stringToFineTune[(string(b))]; ok {
+		*f = val
+		return nil
+	}
+
+	*f = UnknownFineTune
+
+	return nil
+}
 
 var fineTuneToString = map[FineTune]string{
 	Davinci: "davinci",
