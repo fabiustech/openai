42ba6c9de32af73941dd02121e9a5516892cfcc4
Andy Day <aday@fabius.io>
2023-07-05 15:38:12 -0700

    wip

---
+++ models/audio.go
@@ -0,0 +1,39 @@
+package models
+
+type Audio int
+
+const (
+	UnknownAudioModel Audio = iota
+	Whisper1
+)
+
+// String implements the fmt.Stringer interface.
+func (a Audio) String() string {
+	return audioToString[a]
+}
+
+// MarshalText implements the encoding.TextMarshaler interface.
+func (a Audio) MarshalText() ([]byte, error) {
+	return []byte(a.String()), nil
+}
+
+// UnmarshalText implements the encoding.TextUnmarshaler interface.
+// On unrecognized value, it sets |e| to Unknown.
+func (a *Audio) UnmarshalText(b []byte) error {
+	if val, ok := stringToAudio[(string(b))]; ok {
+		*a = val
+		return nil
+	}
+
+	*a = UnknownAudioModel
+
+	return nil
+}
+
+var audioToString = map[Audio]string{
+	Whisper1: "whisper-1",
+}
+
+var stringToAudio = map[string]Audio{
+	"whisper-1": Whisper1,
+}
